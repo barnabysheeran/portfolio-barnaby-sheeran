4daf3e7597e62580610b4db499953af035bb2b1b
Barnaby Sheeran <barnabysheeran@gmail.com>
2025-10-16 17:29:10 +0100

    feat: add fadeQuickVariants and fadeSlowVariants for motion transitions

---
+++ src/ts/motion/fade.ts
@@ -0,0 +1,43 @@
+import type { Variants } from 'framer-motion';
+
+import { DURATION_QUICK, DURATION_SLOW } from './constants';
+
+// _______________________________________________________________________ Quick
+
+export const fadeQuickVariants: Variants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: DURATION_QUICK,
+    },
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      duration: DURATION_QUICK,
+    },
+  },
+};
+
+// ________________________________________________________________________ Slow
+
+export const fadeSlowVariants: Variants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: DURATION_SLOW,
+    },
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      duration: DURATION_SLOW,
+    },
+  },
+};
